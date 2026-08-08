b0a0de7e8db98aefc2817b1fc42363f19eaf9b31
Jacob Sjølie <jacobsjolie42@gmail.com>
2026-08-08 13:59:22 +0200

    add week and calender to waybar

--- home/waybar.nix
+++ home/waybar.nix
@@ -22,8 +22,27 @@
       "hyprland/window".max-length = 60;
 
       clock = {
-        format = "{:%a %d %b  %H:%M}";
+        # %V = ISO week number of the year. Shown as "v.32" (v = vecka/week).
+        format = "{:%a %d %b  v.%V  %H:%M}";
+        # The calendar shows as a tooltip when you hover the clock.
         tooltip-format = "<tt>{calendar}</tt>";
+        calendar = {
+          mode = "month";
+          # Show week numbers down the left side of the calendar too.
+          weeks-pos = "left";
+          on-scroll = 1;
+          format = {
+            # Highlight today; keeps Stylix's theme colors otherwise.
+            today = "<b><u>{}</u></b>";
+            weeks = "<b>v{}</b>";
+          };
+        };
+        actions = {
+          # Right-click toggles month <-> year view; scroll changes months.
+          on-click-right = "mode";
+          on-scroll-up = "shift_up";
+          on-scroll-down = "shift_down";
+        };
       };
 
       battery = {
